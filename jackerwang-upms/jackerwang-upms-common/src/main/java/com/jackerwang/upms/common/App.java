dcf91c5a2899c16a51d7888868c6f47810e41dc1
Jacker-Wang <wangpin@chinatelecom.cn>
2017-11-08 17:41:52 +0800

    生成model

---
+++ jackerwang-upms/jackerwang-upms-common/src/main/java/com/jackerwang/upms/common/App.java
@@ -0,0 +1,13 @@
+package com.jackerwang.upms.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
